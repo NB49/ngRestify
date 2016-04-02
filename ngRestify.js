3e0a529e2ebc165d0dc6908b7a65cdafd9ab84cc
NewZeroRiot <iainplimmer@gmail.com>
2016-04-02 14:41:41 +0100

    Update ngRestify.js

--- ngRestify.js
+++ ngRestify.js
@@ -2,8 +2,6 @@
 //  This is just sugar for the developer to use here. It save's us writing out all this boiler plate every time.
 //  There is a provider setting that allows the user to inject a bearer token too, useful for .NET web API :)  
 
-//  STILL TO DO: Put and delete commands........
-
 (function () {
     angular.module('ngRestify', []) 
         .factory('Restify', _serviceCallers)
@@ -40,6 +38,7 @@
     //  The factory that is used to provide the GET, POST, PUT and DELETE commands
     function _serviceCallers ($http, $q, RestifyApiUrl) {
         
+        //  Create the GET request
         var Get = function (url) {
             var deferred = $q.defer();
             var result = $http.get(RestifyApiUrl.url + url);
@@ -51,14 +50,37 @@
             });
             return deferred.promise;
         };
+        
+        //  Create the DELETE request
+        var Delete = function (url) {
+            var deferred = $q.defer();
+            var result = $http.delete(RestifyApiUrl.url + url);
+            result.then(function (response) {
+                deferred.resolve(response.data);
+            })
+            .catch(function (e) {
+                deferred.reject(e);
+            });
+            return deferred.promise;
+        };
 
-        //  Create the post request that is going to be used system wide
+        //  Create the POST request wrapper that is going to be used system wide
         var Post = function (url, data) {
+            return _withBody(url, data, 'POST');
+        };
+                
+        //  Create the PUT request wrapper that is going to be used system wide
+        var Put = function (url, data) {
+            return _withBody(url, data, 'PUT');
+        };
+        
+        //  Makes a post with a BODY
+        var _withBody = function (url, data, method) {
             var deferred = $q.defer();
             var result = $http({
                 url: RestifyApiUrl.url + url,
                 dataType: 'json',
-                method: 'POST',
+                method: method,
                 data: data
             });
             result.then(function (response) {
@@ -73,7 +95,9 @@
         return {
             //  Revealing module pattern reveals the function
             Get: Get,
-            Post: Post
+            Post: Post,
+            Put: Put,
+            Delete: Delete
         };
     };
 })();
